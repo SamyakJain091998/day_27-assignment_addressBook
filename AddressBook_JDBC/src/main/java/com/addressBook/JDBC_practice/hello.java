0a5c18d019279c17a249c9802f0e3363fac1dba3
SamyakJain091998 <2016224@iiitdmj.ac.in>
2020-10-29 19:46:11 +0530

    Added: An empty repo has been initialized for the addressBook project using JDBC

---
+++ AddressBook_JDBC/src/main/java/com/addressBook/JDBC_practice/hello.java
@@ -0,0 +1,10 @@
+package com.addressBook.JDBC_practice;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world! Welcome to my address book problem...");
+	}
+
+}
